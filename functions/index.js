c6ebfcefc77fc4c753fcc2242e30367c14699029
haho <haho@hotmail.com>
2021-03-18 16:40:57 +0800

    cloud function backup

--- functions/index.js
+++ functions/index.js
@@ -58,3 +58,66 @@ if (collection === 'MDBUsers') {
 return null;
 
 });
+
+
+/*================================================================================================
+ 資料庫備份，利用pub/sub 呼叫cloud function exportDB，再用scheduler 排程執行
+ 要在 Storage 建立值區 gs://BUCKET_NAME
+*/
+
+const firestore = require('@google-cloud/firestore');
+const client = new firestore.v1.FirestoreAdminClient();
+
+/* 
+底下程式碼要修改的部分
+將BUCKET_NAME替換為您的存儲桶的名稱。
+修改every 24 hours以設置您的導出時間表。
+修改collectionIds: ['users', 'posts'] 僅導出指定的集合組。[]導出全部。
+
+cron.yaml參考
+every 24 hours
+every monday 09:00
+every 1 mins
+every 5 minutes
+every 5 minutes from 10:00 to 14:00
+*/
+
+// Replace BUCKET_NAME
+// const bucket = 'gs://BUCKET_NAME';
+const bucket = 'gs://mobiledatabank-backup'; //要改的部分
+
+exports.scheduledFirestoreExport = functions.pubsub
+    .schedule('every 24 hours') //要改的部分
+    .onRun((context) => {
+        const projectId = process.env.GCP_PROJECT || process.env.GCLOUD_PROJECT;
+        const databaseName = client.databasePath(projectId, '(default)');
+
+        return client.exportDocuments({
+            name: databaseName,
+            outputUriPrefix: bucket,
+            // collectionIds: ['users', 'posts']
+            collectionIds: ['現場紀錄表'] //要改的部分 全部備份
+        })
+            .then(responses => {
+                const response = responses[0];
+                console.log(`Operation Name: ${response['name']}`);
+            })
+            .catch(err => {
+                console.error(err);
+                throw new Error('Export operation failed');
+            });
+    });
+
+    /*
+    上面程式執行權限會有問題，到這個頁面操作配置訪問權限，在網頁上先修改project-id後再拷貝，很方便。
+    https://firebase.google.com/docs/firestore/solutions/schedule-export#configure_access_permissions
+  
+    gcloud projects add-iam-policy-binding modatabank \
+    --member serviceAccount:modatabank@appspot.gserviceaccount.com \
+    --role roles/datastore.importExportAdmin
+
+    gsutil iam ch serviceAccount:modatabank@appspot.gserviceaccount.com:admin \
+    gs://mobiledatabank-backup
+
+    上面兩行命令執行完，就OK了！
+    */
